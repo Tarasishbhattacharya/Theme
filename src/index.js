9fbb1ed52fb51133b9e1077c873f94d3d4d0a2cb
Tarasish bhattacharya <tarasishbhattacharya06@gmail.com>
2022-05-26 01:57:21 +0530

    t

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeContextProvider } from './Context/Theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ThemeContextProvider>
     <App />
-  </React.StrictMode>
+  </ThemeContextProvider>
+    
+
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
